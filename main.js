8057abcb4b44f9ef99f7a0cfd6faabf9ed69427f
Facundo A Grana <fgrana001@nam.ad.pwcinternal.com>
2018-07-26 16:53:09 -0400

    Added some logic

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var app = express();
 var firebase = require('firebase');
 var path = require("path");
 var fs = require('fs');
+var bodyParser = require('body-parser');
 
 // Initialize Firebase
 var config = {
@@ -15,15 +16,8 @@ var config = {
 };
 firebase.initializeApp(config);
 
-//Get Week
-Date.prototype.getWeek = function () {
-    var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
-    var dayNum = d.getUTCDay() || 7;
-    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
-    var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-    return Math.ceil((((d - yearStart) / 86400000) + 1) / 7)
-};
 
+//Write hours to DB
 function writeHours(uid, date, leave, hours) {
     // An hour entry.
     var week = date.getWeek() - 1;
@@ -45,16 +39,34 @@ function writeHours(uid, date, leave, hours) {
 
     return firebase.database().ref().update(updates);
 }
-date = new Date();
-uid = 1529604;
-writeHours(uid, date, false, 8);
+//Get Week
+Date.prototype.getWeek = function () {
+    var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+    var dayNum = d.getUTCDay() || 7;
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    return Math.ceil((((d - yearStart) / 86400000) + 1) / 7)
+};
 
 app.use('/js', express.static('js'));
 app.use('/img', express.static('img'));
 app.use('/css', express.static('css'));
 app.use('/time_web_app', express.static('time_web_app'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
+
+app.post('/submit', (req, res) => {
+    var uid = req.body.uid;
+    var date = req.body.dateEntry;
+    var leave = false;
+    var hours = req.body.hours;
+    console.log(req.body)
+    /* writeHours(uid, date, leave, hours); */
+})
 app.listen(8080, () => console.log('server started at localhost'));
